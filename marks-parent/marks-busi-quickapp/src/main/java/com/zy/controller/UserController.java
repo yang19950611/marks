984e7cbc6d6dafd311cf0029a64c3ec9886123f4
yhy19950611 <751433983@qq.com>
2018-10-07 17:41:37 +0800

    配置中心客户端测试

--- marks-parent/marks-busi-quickapp/src/main/java/com/zy/controller/UserController.java
+++ marks-parent/marks-busi-quickapp/src/main/java/com/zy/controller/UserController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +62,13 @@ public class UserController {
         String str = testService.testHy();
         logger.info("获取结果:"+str);
         return str;
+    }
+
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
 }
